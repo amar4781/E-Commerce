39c9d435028c68d24792cc54a4ba2db40b03d93e
amar4781 <ammarfarouk2001@gmail.com>
2025-01-02 15:27:19 +0200

    update commit

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,7 +12,7 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        return $request->expectsJson() ? null : route('admin.login');
 //        if (!$request->expectsJson()){
 //            if (Request::is('admin/*'))
 //                    return route('admin/login');
